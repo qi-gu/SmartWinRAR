d24514f795954e716f099225d395c0ca026fe7cc
qi_gu <252915823@qq.com>
2026-08-19 16:17:52 +0800

    Add support for 7z

--- src/Installer.cs
+++ src/Installer.cs
@@ -25,7 +25,7 @@ internal static class InstallerProgram
     // independent of the file association's optional PerceivedType value.
     private static readonly string[] ArchiveExtensions =
     {
-        ".zip", ".rar", ".tar", ".gz", ".tgz",
+        ".zip", ".rar", "7z", ".tar", ".gz", ".tgz",
         ".jar", ".apk", ".epub", ".cbz",
         ".bz2", ".xz", ".zst", ".lz", ".lzma"
     };
